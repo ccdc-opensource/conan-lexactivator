60bec499d30b7f86d5cce3885269c423e9f24b24
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2020-06-18 00:36:05 +0300

    fix test

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -2,7 +2,15 @@
 #include <stdlib.h>
 
 int main() {
-    SetProductFile("invalid");
+#if _WIN32
+	int status = SetProductData(L"foo");
+#else
+	int status = SetProductData("bar");
+#endif
+	if (LA_OK == status)
+	{
+		return EXIT_FAILURE;
+	}
     return EXIT_SUCCESS;
 }
 
