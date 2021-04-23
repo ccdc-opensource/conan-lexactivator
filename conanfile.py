ed63bbd22ae78909b42f834fa8bf9c00510ccd52
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2021-04-23 12:11:58 +0100

    Install epel-release on rh like distributions only

--- conanfile.py
+++ conanfile.py
@@ -79,7 +79,8 @@ class ConanLexActivator(ConanFile):
     def system_requirements(self):
         if tools.os_info.is_linux:
             installer = tools.SystemPackageTool()
-            installer.install(f"epel-release")
+            if tools.os_info.linux_distro == "fedora" or tools.os_info.linux_distro == "centos" or tools.os_info.linux_distro == "redhat":
+                installer.install(f"epel-release")
             installer.install(f"patchelf")
 
     def package(self):
